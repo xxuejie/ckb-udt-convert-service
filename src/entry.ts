3f11d2171d1be6a156c46fc221bcf0b0467e94a4
Xuejie Xiao <xxuejie@gmail.com>
2025-08-01 03:07:40 +0000

    Piece rpc server with a simple express HTTP endpoint

--- src/entry.ts
+++ src/entry.ts
@@ -1,6 +1,9 @@
+import express from "express";
+
 import { funder } from "./env";
 import "./workers";
 import "./signer";
+import { rpc } from "./rpc";
 
 async function init() {
   const tip = await funder.client.getTipHeader();
@@ -9,6 +12,21 @@ async function init() {
   console.log("balance: ", balance);
   console.log("addresses: ", await funder.getAddresses());
   // queue.add("refresh", {});
+
+  const app = express();
+  app.use(express.json());
+
+  app.post("/rpc", (req, res) => {
+    rpc.receive(req.body).then((resp) => {
+      if (resp) {
+        res.json(resp);
+      } else {
+        res.sendStatus(204);
+      }
+    });
+  });
+
+  app.listen(process.env["PORT"] || 8000);
 }
 
 init();
